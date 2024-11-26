8014849454daa676168c94b13fd3f6b158bf61d9
RodrikKlesse <rodrik_hsk@hotmail.com>
2024-11-26 09:17:58 -0300

    Initialize project

---
+++ src/test/java/com/klesse/proposal_app/ProposalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klesse.proposal_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
